146055445c35e101bdbe9e2d8894071acfa27a09
Sterling Orsten <sgorsten@gmail.com>
2014-09-20 02:42:37 -0700

    Support a basic Dijkstra search algorithm.

--- src/search.cpp
+++ src/search.cpp
@@ -5,6 +5,9 @@
 #pragma comment(lib, "opengl32.lib")
 
 #include <vector>
+#include <algorithm>
+
+struct int2 { int x,y; };
 
 class Map
 {
@@ -17,6 +20,57 @@ public:
     int GetHeight() const { return height; }
     bool IsObstruction(int x, int y) const { return tiles[y*width+x] != 0; }
     void SetObstruction(int x, int y, bool isObstruction) { tiles[y*width+x] = isObstruction ? 1 : 0; }
+
+    std::vector<int2> Search(const int2 & start, const int2 & goal) const
+    {
+        const int2 directions[] = {{1,0},{1,1},{0,1},{-1,1},{-1,0},{-1,-1},{0,-1},{1,-1}};
+        const int costs[] = {5,7,5,7,5,7,5,7};
+
+        struct OpenNode { int2 state; int lastAction, gCost; bool operator < (const OpenNode & r) const { return r.gCost < gCost; } };
+        std::vector<OpenNode> open;
+        std::vector<int> closed(tiles.size(), -1);
+
+        open.push_back({start, 0, 0});
+        while(!open.empty())
+        {
+            auto node = open.front();
+            std::pop_heap(begin(open), end(open));
+            open.pop_back();
+
+            auto state = node.state;
+            if(closed[state.y * width + state.x] != -1) continue;
+
+            closed[state.y * width + state.x] = node.lastAction;
+            if(state.x == goal.x && state.y == goal.y)
+            {
+                std::vector<int2> path(1,state);
+                while(state.x != start.x || state.y != start.y)
+                {
+                    
+                    int action = closed[state.y * width + state.x];
+                    state.x -= directions[action].x;
+                    state.y -= directions[action].y;
+                    path.push_back(state);
+                }
+                std::reverse(begin(path), end(path));
+                return path;
+            }
+
+            for(int i=0; i<8; ++i)
+            {
+                auto newState = state;
+                newState.x += directions[i].x;
+                newState.y += directions[i].y;
+                if(newState.x < 0 || newState.y < 0 || newState.x >= width || newState.y >= height) continue;
+                if(IsObstruction(newState.x, newState.y)) continue;
+                if(IsObstruction(state.x, newState.y) && IsObstruction(newState.x, state.y)) continue;
+                open.push_back({newState, i, node.gCost + costs[i]});
+                std::push_heap(begin(open), end(open));
+            }
+        }
+
+        return {};
+    }
 };
 
 #include <iostream>
@@ -37,6 +91,8 @@ int main() try
     map.SetObstruction(38,2,true);
     map.SetObstruction(6,27,true);
 
+    int2 startTile;
+    bool middleClicked = false;
     while(!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
@@ -63,7 +119,17 @@ int main() try
             {
                 map.SetObstruction(tileX, tileY, false);
             }
+
+            if(!middleClicked && glfwGetMouseButton(window, 2) == GLFW_PRESS)
+            {
+                startTile = {tileX, tileY};
+                middleClicked = true;
+            }
         }
+        if(glfwGetMouseButton(window, 2) == GLFW_RELEASE) middleClicked = false;
+
+        std::vector<int2> path;
+        if(middleClicked) path = map.Search(startTile, {tileX,tileY});
 
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
@@ -71,7 +137,8 @@ int main() try
         glPushMatrix();
         glOrtho(0, width, height, 0, -1, +1);
         glColor3f(1,1,0);
-        font.Print(32, 32, "Left-click to add obstruction, right-click to clear obstruction.", 8, 14);
+        font.Print(32, 32, "Left-click to add obstruction, right-click to clear obstruction.");
+        font.Print(32, 48, "Middle-click and drag to find a path between two points.");
 
         glPushMatrix();
         glTranslated(mapOffsetX, mapOffsetY, 0);
@@ -95,6 +162,31 @@ int main() try
             }
         }
         glEnd();
+
+        if(middleClicked)
+        {
+            glBegin(GL_LINE_STRIP);
+            glColor3f(1,1,0);
+            for(auto tile : path) glVertex2i(tile.x * mapPixelScale + mapPixelScale/2, tile.y * mapPixelScale + mapPixelScale/2);
+            glEnd();
+
+            glBegin(GL_QUADS);
+            glColor3f(0,1,0);
+            glVertex2d(startTile.x*mapPixelScale + mapPixelScale/2 - 2, startTile.y*mapPixelScale + mapPixelScale/2-2);
+            glVertex2d(startTile.x*mapPixelScale + mapPixelScale/2 + 2, startTile.y*mapPixelScale + mapPixelScale/2-2);
+            glVertex2d(startTile.x*mapPixelScale + mapPixelScale/2 + 2, startTile.y*mapPixelScale + mapPixelScale/2+2);
+            glVertex2d(startTile.x*mapPixelScale + mapPixelScale/2 - 2, startTile.y*mapPixelScale + mapPixelScale/2+2);
+            glEnd();
+
+            /*glBegin(GL_QUADS);
+            glColor3f(0,1,1);
+            glVertex2d(startTile.x-2, startTile.y-2);
+            glVertex2d(startTile.x+2, startTile.y-2);
+            glVertex2d(startTile.x+2, startTile.y+2);
+            glVertex2d(startTile.x-2, startTile.y+2);
+            glEnd();*/
+        }
+
         glPopMatrix();
 
         glBegin(GL_QUADS);
